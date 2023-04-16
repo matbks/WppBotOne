61b885fdaa0ff3f03249c2373cd80786f242aa8f
Backhaus <44726485+matbks@users.noreply.github.com>
2023-04-16 18:33:10 -0300

    date and time wpp message

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.post("/register", (req, res) => {
     console.log(correctNumber)
   if (correctNumber) {
   
-    client.sendMessage(correctNumber, "Obrigado! Seu ponto foi registrado agora " + today() + " às " + now() + ".");
+    client.sendMessage(correctNumber, "Obrigado! Seu ponto foi registrado agora " + today() + " às " + Date().toLocaleTimeString() + ".");
   } else {
     console.log("Falha no registro");
   }
